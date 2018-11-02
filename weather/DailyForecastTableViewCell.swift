68303dd8e065c05abb4e5d755e9019b9db9b0218
BobbyLiu66 <bobbyliu45@gmail.com>
2018-11-03 10:30:38 +1300

    delete unused file

--- weather/DailyForecastTableViewCell.swift
+++ weather/DailyForecastTableViewCell.swift
@@ -18,12 +18,12 @@ class DailyForecastTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
+        
     }
 
 }
